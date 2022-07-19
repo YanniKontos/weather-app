d8abd16a433edcc7e16b231c4af63a9662e65670
TheNarcissist1 <unreal810@gmail.com>
2022-07-19 23:31:37 +0100

    more changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=4c553b35fe5bd4983d58eba9c3fe5c6c`
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=4c553b35fe5bd4983d58eba9c3fe5c6c`;
 
 fetch(apiUrl)
 
@@ -8,23 +8,45 @@ fetch(apiUrl)
     const tempDisplay = document.getElementById('temp-display');
     const humidityDisplay = document.getElementById('humidity-display'); 
     const conditionDisplay = document.getElementById('condition-display');
-    const cloudIcon = document.getElementById('cloudIcon');
+    const cloudIcon = document.getElementById('icon8');
     const sunIcon = document.getElementById('sunIcon');
 
     let tempValue = (data.main.temp);
     let humidity = (data.main.humidity);
-    let precipitation = (data.weather[0].description)
+    let precipitation = (data.weather[0].description);
 
     tempDisplay.innerHTML = `Temp: <br> ${Math.round(tempValue)}°C `;
     humidityDisplay.innerHTML = `Humidity: <br> ${humidity}%`;
     conditionDisplay.innerHTML = `Condition: <br> ${precipitation}`;
     
-//    if (precipitation == 'clear sky') {
-        
-    
-//    }
-    
-    console.log(precipitation);
-    console.log(data);
-});
+    function changeIcon() {
+        if (precipitation === 'broken clouds' || precipitation === 'scattered clouds' ||
+            precipitation === 'few clouds') {
+            cloudIcon.src = '/visual media/cloud.png' 
+        }
 
+        else if (precipitation === 'clear sky') {
+            cloudIcon.src ='/visual media/sun.png' 
+        }
+
+        else if (precipitation === 'shower rain' || precipitation === 'rain') {
+            cloudIcon.src ='/visual media/rain.png' 
+        }
+
+        else if (precipitation === 'thunderstorm') {
+            cloudIcon.src ='/visual media/thunder.png' 
+        }
+
+        else {
+            cloudIcon.src ='/visual media/snow.png' 
+        };
+        
+    };
+
+    // else if spaghetti code , much help appreciated !
+
+    changeIcon()
+    
+    console.log(data);
+
+});
